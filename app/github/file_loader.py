c1ef4078650c6ffd90b51a89107fb865c0fd2c6e
HemanthKumarKS <155090119+hemanthshashidhar@users.noreply.github.com>
2026-07-02 19:15:16 +0000

    Update file_loader.py

--- app/github/file_loader.py
+++ app/github/file_loader.py
@@ -15,7 +15,7 @@ def load_repository(repo_path):
 
     documents = []
 
-    for root, dirs, files in os.walk(repo_path): 
+    for root, dirs, files in  os.walk(repo_path): 
 
         dirs[:] = [
             d for d in dirs
